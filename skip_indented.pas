c9f02cbc53b4f508aebbb7eab299afb011affa8c
nabbbbi <nabbbbi@mail.ru>
2022-02-27 14:35:31 +0300

    some changes

--- skip_indented.pas
+++ skip_indented.pas
@@ -4,25 +4,25 @@ var
    print,know : boolean;
    begin
       print:=false;
-      know:=false;
+      know:=false; 
       while not eof do
 	 begin
 	    read(c);
-	    if c = #10 then
+	    if c = #10 then {enter}
 	       begin
-		  if print and know then
+		  if print and know then {}
 		     writeln;
-		  know:=false
+		  know:=false {are we printing?}
 	       end
 		  else
 		     begin
 		     if not know then
 			begin
-			   print:= (c <> ' ') and (c <> #9);
-			   know:=true
+			   print:= (c <> ' ') and (c <> #9); {any symbol barring "space" and "tabulation"}
+			   know:=true {Yes! We printing}
 			end;
 		  if print then
 		     write(c)
-	       end;
-	 end;
+	       end
+	 end
    end.
