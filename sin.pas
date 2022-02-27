2df98bd50f50510485e81ae47e17ba33d2f5c264
nabbbbi <nabbbbi@mail.ru>
2022-02-27 11:21:52 +0300

    added commentaries

--- sin.pas
+++ sin.pas
@@ -1,15 +1,15 @@
 program sinzero;
 type
-   real100 = array [1..100] of real;
+   real100 = array [1..100] of real; {describe type of massive}
    var
-      a,b : real100;
+      a,b : real100; {we always should initialize the massive describing type of massive}
       i	  : integer;
       begin
 	 a[1]:=1;
 	 for i:=2 to 100 do
-	    a[i]:=a[i-1]/2;
+	    a[i]:=a[i-1]/2; {in each index of massive we divide predecesor index of massive by 2. For example. a[3]= a[2]/2. where a[2] = 1/2} 
 	 for i:=1 to 100 do
-	    b[i]:=sin(a[i]);
+	    b[i]:=sin(a[i]); {put all results of a massive ot b massive}
 	 for i:=1 to 100 do
 	    writeln(a[i],#9,b[i])
       end.
--- test.pas
+++ test.pas
@@ -1,14 +1,22 @@
 program plain_numbers;
 procedure plain (n : integer);
+type
+   arr	= record
+	     ii	: integer;
+	     rr	: integer;
+	  end;	
+   arr1	= array [1..10] of arr;
 var
-   r,i,j : integer;
+   {r,}i,j : integer;
+   t	 : arr1;
    begin
       for i:=2 to n do
 	 begin
 	    for j:=1 to i do
 	       begin
-		  r:=i mod j;
-		  writeln(i,' ',r)
+		  t[j].rr:= i mod j;
+		  t[j].ii:=i;
+		  writeln(t[j].ii,' ',t[j].rr)
 	       end;
 	 end
    end;
